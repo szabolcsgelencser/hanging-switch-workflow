be88acc43a576bd6128b9f4f9bafedec77289ce5
Szabolcs Gelencsér <szabolcs.gelencser@bitrise.io>
2022-07-25 08:15:49 +0200

    asd

--- main.go
+++ main.go
@@ -7,26 +7,9 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"time"
 )
 
 func main() {
-	go func() {
-		var n int
-		for {
-			ctx := context.Background()
-			host := fmt.Sprintf("dns-lookup-%d", n)
-			_, err := net.DefaultResolver.LookupIPAddr(ctx, host)
-			if err != nil {
-				// fmt.Printf("error: lookup host: %s\n", err)
-			} else {
-				// fmt.Printf("dns addresses: %+v\n", addrs)
-			}
-			time.Sleep(100 * time.Millisecond)
-			n++
-		}
-	}()
-
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 	var n int
@@ -39,6 +22,18 @@ func main() {
 		}
 
 		n++
+
+		go func() {
+			ctx := context.Background()
+			host := fmt.Sprintf("dns-lookup-%d", n)
+			_, err := net.DefaultResolver.LookupIPAddr(ctx, host)
+			if err != nil {
+				// fmt.Printf("error: lookup host: %s\n", err)
+			} else {
+				// fmt.Printf("dns addresses: %+v\n", addrs)
+			}
+		}()
+
 		cwd, _ := os.Getwd()
 		if err := EnvmanInitAtPath(filepath.Join(cwd, "envstore")); err != nil {
 			fmt.Printf("error: envman init: %s\n", err)
